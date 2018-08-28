d4a07ea04f05580651d02b0fafeadca55ad6bde5
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-28 09:52:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
     # the cash register was initialized with an employee discount
     if discount > 0 
       @total -= @total*@discount/100
-      "After the discount, the total comes to $800."
+      "After the discount, the total comes to $#{total}."
     else
       return "There is no discount to apply."
     end 
