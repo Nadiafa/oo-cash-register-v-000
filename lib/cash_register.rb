37739a9bab067addc34f18ab931204359cf6711b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-28 09:41:48 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,13 +14,12 @@ class CashRegister
   def apply_discount
     discount_amount = @total * @discount/100
     @total -= discount_amount 
-    if discount > 0
-      puts "Your #{@discount}% discount has been applied."
-      puts "After the discount, the total comes to #{@total}."
-    else
-      
-    end 
   end
 
+  def cash_register_with_discount
+    sef.apply_discount
+    puts "Your #{@discount}% discount has been applied."
+    puts "After the discount, the total comes to #{@total}."
+  end 
   
 end 
