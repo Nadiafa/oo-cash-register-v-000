868264800d6d5e64cf700b4f5412166554169c25
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-28 09:46:38 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,17 +11,17 @@ class CashRegister
     @total += price * quantity
   end 
   
-  # def apply_discount
-  #   # the cash register was initialized with an employee discount
-  #   if discount > 0 
-      
+  def apply_discount
+    # the cash register was initialized with an employee discount
+    if discount > 0 
+      @total -= @total*@discount/100
   #   end 
     #   applies the discount to the total price
     #   returns success message with updated total (FAILED - 1)
     #   reduces the total
     # the cash register was not initialized with an employee discount
     #   returns a string error message that there is no discount to apply
-  # end
+  end
   
   # def apply_discount
   #   discount_amount = @total * @discount/100
