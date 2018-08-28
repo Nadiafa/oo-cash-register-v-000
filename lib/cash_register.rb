1bbe35c2fdcea51dc14d29f520965fa8e35c9427
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-28 09:17:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  
+  attr_accessor :discount, :total , :items
 
+  def initialize(discount=20)
+    @total = 0
+    @discount = discount 
+  end 
+
+  def add_item(title, price, quantity=1)
+
+  end 
+
+  
+end 
